bad0fd0698e63e960de3fd7d208d49aa1a6036e5
hasanaat <arsheema619@gmail.com>
2025-11-07 11:20:15 +0530

    build

---
+++ server/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { compileFunction } from "vm";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 5001;
+
+app.use(express.json());
+
+const buildPath = path.join(__dirname, "build");
+if (fs.existsSync(buildPath)){
+app.use(express.static(buildPath));
+app.get(/^\/(?!api).*/,(req, res)=>{
+    res.sendFile(path.join(buildPath, "index.html"));
+})
+}
+
+app.get("/api/test", (req, res)=>{
+res.json({ message: "Hello from /api/test!"});
+})
+
+// error handler
+app.use((req, res)=>{
+    res.status(404).send("404 - Not Found");
+});
+
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send("500 - Internal Server Error");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+});
